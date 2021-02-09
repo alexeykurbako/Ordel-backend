30ce1157e7847a62502923a00cdc82379ff54777
Markus <36738501+Markus@users.noreply.github.com>
2021-02-09 21:41:21 +0300

    remove watermarks

--- src/api/common/client/clientService.js
+++ src/api/common/client/clientService.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) Akveo 2020. All Rights Reserved.
- * Licensed under the Single Application / Multi Application License.
- * See LICENSE_SINGLE_APP / LICENSE_MULTI_APP in the 'docs' folder for license information on type of purchased license.
- */
 
 const ClientsRepository = require('./clientRepository');
 const helper = require('../../../utils/helper');
